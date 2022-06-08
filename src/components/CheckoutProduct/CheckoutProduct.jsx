945ad24ec7d85a459ad74e96e3731171516a52e2
vivek <vivekkumarvky13@gmail.com>
2022-06-08 15:34:33 +0530

    checkoutupdate

--- src/components/CheckoutProduct/CheckoutProduct.jsx
+++ src/components/CheckoutProduct/CheckoutProduct.jsx
@@ -13,7 +13,30 @@ export const CheckoutProduct = ({ id, title, image, rating, price, hideButton })
 
   return (
     <div className="checkout-product">
-  
+      <img src={image} alt="" className="checkout-product-image" />
+      <div className="checkout-product-info">
+        <p className="checkout-product-title">{title}</p>
+        <p className="checkout-product-price">
+          <strong>$</strong>
+          <strong>{price}</strong>
+        </p>
+        <div></div>
+        <div className="checkout-product-rating">
+          {Array(rating)
+            .fill()
+            .map((_, index) => (
+              <p key={index}>⭐</p>
+            ))}
+        </div>
+        {!hideButton && (
+          <button onClick={removeItemFromBasket}>
+            <i>
+              <ShoppingCartOutlinedIcon />
+            </i>
+            Remove from Basket
+          </button>
+        )}
+      </div>
     </div>
   );
 };
